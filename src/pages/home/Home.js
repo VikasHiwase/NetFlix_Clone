8143a45285a30a35d112a9d5a12b32d205a42c03
vikas hiwase <vikas.hiwase20@gmail.com>
2022-02-07 16:46:34 +0530

    initialize app navbar & featured component added with home page

---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Featured from "../../components/featured/Featured";
+import Navbar from "../../components/navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      <Featured type="movie" />
+    </div>
+  );
+};
+
+export default Home;
